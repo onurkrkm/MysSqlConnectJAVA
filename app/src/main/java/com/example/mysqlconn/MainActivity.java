371274a445e2aacca7faa9ea0c03a9c2ef0e4f61
user <onurkrkm@gmail.com>
2022-02-06 04:15:16 +0300

    JAVA MySQL Connect with PHP

---
+++ app/src/main/java/com/example/mysqlconn/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.mysqlconn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt1,bt2;
+    TextView tv1;
+    EditText edt1,edt2;
+    RequestQueue requestQueue ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1= findViewById(R.id.button);
+        bt2= findViewById(R.id.button2);
+        tv1= findViewById(R.id.tv);
+        edt1= findViewById(R.id.edt);
+        edt2=findViewById(R.id.edt2);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setText("");
+
+                String showUrl = "http://192.168.0.12/showUsers.php";
+                requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                        showUrl, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        System.out.println(response.toString());
+                        try {
+
+                            URL url = new URL(showUrl);
+                            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                            http.setRequestMethod("POST");
+                            JSONArray jsonArray = response.getJSONArray("showUser");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                String id= jsonObject.getString("id");
+                                String name= jsonObject.getString("name");
+                                String lastname= jsonObject.getString("lastname");
+                                tv1.append(id+"."+"Name :"+name+"Lastname :"+lastname+"\n");
+
+
+                            }
+
+
+                        } catch (JSONException | IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.append(error.getMessage());
+
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+
+
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                requestQueue = Volley.newRequestQueue(getApplicationContext());
+                String insertUrl = "http://192.168.0.12/insertDb.php";
+
+                StringRequest update = new StringRequest(Request.Method.POST,insertUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> parameters  = new HashMap<String, String>();
+                        String sql="INSERT INTO `users` (`id`, `name`, `lastname`) VALUES (NULL, '"+edt1.getText().toString()+"', '"+edt2.getText().toString()+"');";
+
+                        parameters.put("sql", sql);
+
+
+                        return parameters;
+                    }
+                };
+                requestQueue.add(update);
+
+
+
+
+            }
+        });
+
+
+    }
+}
