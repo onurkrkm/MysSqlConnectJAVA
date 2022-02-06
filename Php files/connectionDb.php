fc645a6519357a2970d6d8188c4296384196e152
onurkrkm <63620246+onurkrkm@users.noreply.github.com>
2022-02-06 04:37:17 +0300

    Update connectionDb.php

--- Php files/connectionDb.php
+++ Php files/connectionDb.php
@@ -1,9 +1,9 @@
 <?php
  
-define('hostname', 'localhost');  
-define('user', 'root');
-define('password', '');
-define('databaseName', 'yeni');
+define('hostname', '___-localhost___');  
+define('user', '__-username___');
+define('password', '___password___');
+define('databaseName', '____yourdatabase name__-');
  
  
 $connect = mysqli_connect(hostname, user, password, databaseName);
